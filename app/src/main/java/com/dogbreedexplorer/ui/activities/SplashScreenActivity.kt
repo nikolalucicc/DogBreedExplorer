accefcae27c70f5933234a53580e4c7277acd87f
NIkola Lucic <lucic2612@gmail.com>
2023-10-02 17:09:37 +0200

    Splash screen and logo

---
+++ app/src/main/java/com/dogbreedexplorer/ui/activities/SplashScreenActivity.kt
@@ -0,0 +1,23 @@
+package com.dogbreedexplorer.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.ComponentActivity
+import com.dogbreedexplorer.MainActivity
+import com.dogbreedexplorer.R
+
+class SplashScreenActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen_layout)
+
+        val delayMillis: Long = 2000
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, delayMillis)
+    }
+}
